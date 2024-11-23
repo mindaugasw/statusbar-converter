b7d5517704fdfd7d7c8888bc7c9596e70069e624
Mindaugas <mindaugas932@gmail.com>
2024-11-23 17:09:12 +0200

    Add validation if Clipnotify binary exists

--- src/Service/ClipboardManagerLinux.py
+++ src/Service/ClipboardManagerLinux.py
@@ -2,10 +2,21 @@ import os
 import subprocess
 import threading
 from src.Service.ClipboardManager import ClipboardManager
+from src.Service.Debug import Debug
 from src.Service.FilesystemHelperLinux import FilesystemHelperLinux
 
 
 class ClipboardManagerLinux(ClipboardManager):
+    _clipnotifyPath: str
+
+    def __init__(self, debug: Debug):
+        super().__init__(debug)
+
+        self._clipnotifyPath = FilesystemHelperLinux.getBinariesDir() + '/clipnotify/clipnotify'
+
+        if not os.path.isfile(self._clipnotifyPath):
+            raise Exception('Clipnotify binary not found in: ' + self._clipnotifyPath)
+
     def initializeClipboardWatch(self) -> None:
         threading.Thread(target=self._watchClipboard, daemon=True).start()
 
@@ -14,13 +25,11 @@ class ClipboardManagerLinux(ClipboardManager):
         subprocess.run(['xsel', '-ip'], input=content, text=True)
 
     def _watchClipboard(self) -> None:
-        clipnotifyPath = FilesystemHelperLinux.getBinariesDir() + '/clipnotify/clipnotify'
-
         while True:
             # Clipnotify will block thread until selection changes, so we run command and simply wait.
             # To allow clipnotify to block, it must be run with `call`
             # See https://stackoverflow.com/a/2562292/4110469
-            subprocess.call([clipnotifyPath], stdout=None, stderr=None)
+            subprocess.call([self._clipnotifyPath], stdout=None, stderr=None)
 
             selection = os.popen('xsel -o').read()
             # selection = os.popen('xsel -obp').read()
